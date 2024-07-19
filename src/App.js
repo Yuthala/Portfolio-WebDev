ed74750843bf9993b5f4c5d50b79b351c68b0103
Yuthala <dinapolousova@gmail.com>
2024-07-19 21:22:31 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/main.css";
+
+import vk from "./img/icons/vk.svg";
+import instagram from "./img/icons/instagram.svg";
+import twitter from "./img/icons/twitter.svg";
+import linkedIn from "./img/icons/linkedIn.svg";
+import gitHub from "./img/icons/gitHub.svg";
+
+import Navbar from './components/navbar/Navbar';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <Navbar />
+
+      <Header />
+
+      <main className="section">
+          <div className="container">
+
+                  <ul className="content-list">
+                      <li className="content-list__item">
+                          <h2 className="title-2">Frontend</h2>
+                          <p>JavaScript, TypeScript, ReactJS, Angular, Redux, HTML, CSS, NPM, BootStrap, MaterialUI, Yarn, TailwindCSS, StyledComponents</p>
+                      </li>
+                      <li className="content-list__item">
+                          <h2 className="title-2">Backend</h2>
+                          <p>NodeJS, MySQL, MongoDB, PHP, Laravel</p>
+                      </li>
+                  </ul>
+
+          </div>
+      </main>
+
+      <footer className="footer">
+          <div className="container">
+              <div className="footer__wrapper">
+                  <ul className="social">
+                      <li className="social__item"><a href="#!"><img src={vk} alt="Link" /></a></li>
+                      <li className="social__item"><a href="#!"><img src={instagram} alt="Link" /></a></li>
+                      <li className="social__item"><a href="#!"><img src={twitter} alt="Link" /></a></li>
+                      <li className="social__item"><a href="#!"><img src={gitHub} alt="Link" /></a></li>
+                      <li className="social__item"><a href="#!"><img src={linkedIn} alt="Link" /></a></li>
+                  </ul>
+                  <div className="copyright">
+                      <p>© 2022 frontend-dev.com</p>
+                  </div>
+              </div>
+          </div>
+      </footer>
+
     </div>
   );
 }
 
 export default App;
 
-// save me now
