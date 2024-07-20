1def8c3d97c2086c8e727b6c08d3a0b3d3ddaebb
Yuthala <dinapolousova@gmail.com>
2024-07-20 14:52:07 +0300

    scrollToTop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import "./styles/main.css";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
-import ProjectItem from "./pages/Project";
+import ProjectItem from "./pages/ProjectItem";
 import Contacts from "./pages/Contacts";
+
+import ScrollToTop from "./utils/ScrollToTop";
 
 
 function App() {
   return (
     <div className="App">
-     
-      <Navbar />
+     <Router>
+      <ScrollToTop/>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/projects" element={<Projects />}/>
+          <Route path="/project" element={<ProjectItem />}/>
+          <Route path="/contacts" element={<Contacts />}/>
+        </Routes>
+        <Footer />
+     </Router>
 
-      {/* <Home /> */}
-      {/* <Projects /> */}
-      <ProjectItem />
-      {/* <Contacts /> */}
-
-      <Footer />
 
     </div>
   );
